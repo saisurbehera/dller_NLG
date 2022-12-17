8df7921894cf48ca6dfa39337652712c05ad6e47
root <ss6365@columbia.edu>
2022-12-17 00:07:27 +0000

    added RETRO baseline

---
+++ models/RETRO/setup.py
@@ -0,0 +1,35 @@
+from setuptools import setup, find_packages
+
+setup(
+  name = 'retro-pytorch',
+  packages = find_packages(exclude=[]),
+  version = '0.3.8',
+  license='MIT',
+  description = 'RETRO - Retrieval Enhanced Transformer - Pytorch',
+  long_description_content_type = 'text/markdown',
+  author = 'Phil Wang',
+  author_email = 'lucidrains@gmail.com',
+  url = 'https://github.com/lucidrains/RETRO-pytorch',
+  keywords = [
+    'artificial intelligence',
+    'deep learning',
+    'transformers',
+    'attention-mechanism',
+    'retrieval',
+  ],
+  install_requires=[
+    'autofaiss',
+    'einops>=0.3',
+    'numpy',
+    'sentencepiece',
+    'torch>=1.6',
+    'tqdm'
+  ],
+  classifiers=[
+    'Development Status :: 4 - Beta',
+    'Intended Audience :: Developers',
+    'Topic :: Scientific/Engineering :: Artificial Intelligence',
+    'License :: OSI Approved :: MIT License',
+    'Programming Language :: Python :: 3.6',
+  ],
+)
